7d00b8bee0dd7b4d0fdbda4681bfcef1901f6c47
Brent W <kalamazpi@gmail.com>
2018-01-18 23:01:03 -0600

    Generated unique list of log types with sorted keys

--- logParse.js
+++ logParse.js
@@ -62,81 +62,21 @@ rl.on("line", function(line) {
 
 // On file close, we process the array and perform the work of the program.
 rl.on("close", function() {
-    let myInterestingKeys = [
-        {hasKey:"msg",keyIs:"development env",count:0}, 
-        {hasKey:"msg",keyIs:"commit SET_AC_PRESENT",otherThingToPrint:"payload",count:0},
-        {hasKey:"error",count:0},
-        {hasKey:"logIssue",count:0}
-    ];
-    // loop through log array and process
-    // TODO: Add separate step to "mark" interesting log entries and then iterate through the mark array to print the log entries of interest.
-    // Find the following:
-    // 1. Any instances of log corruption or NULLs
-    // 2. All "development env"
-    // 3. ALL "SET_AC_PRESENT" along with its value
-    // 4. All "error"
-    // TODO: Need to add "msg" that contains "error" to get AWS errors.
-    // For all, count all occurrences (count payloads of SET_AC_PRESENT separately from each other)
-    // TODO: Clean up the Rube-Golberg logic below.  It's an embarrassment.
-    for (i = 0; i < logArray.length; i += 1) {
-        let myStringToPrint = "";
-        for (j = 0; j < myInterestingKeys.length; j += 1) {
-            if (myInterestingKeys[j].hasKey in logArray[i]) {
-                if ("keyIs" in myInterestingKeys[j]) {
-                    // Here we handle Interesting Keys which require the key to have a specific value
-                    if (myInterestingKeys[j].keyIs === logArray[i][myInterestingKeys[j].hasKey]) {
-                        myInterestingKeys[j].count += 1;
-                        // Always try to print the time, and print it first.
-                        if ("time" in logArray[i]) {
-                            myStringToPrint = logArray[i].time;
-                        }
-                        if (myInterestingKeys[j].hasKey in logArray[i]) {
-                            myStringToPrint += ", " + myInterestingKeys[j].hasKey + ": ";
-                            // Handle logArray payloads that are still objects (errors).
-                            if (typeof(logArray[i][myInterestingKeys[j].hasKey]) == "object") {
-                                myStringToPrint += JSON.stringify(logArray[i][myInterestingKeys[j].hasKey]);
-                            } else {
-                                myStringToPrint += logArray[i][myInterestingKeys[j].hasKey];
-                            }                  
-                            if ("otherThingToPrint" in myInterestingKeys[j]) {
-                                if (myInterestingKeys[j].otherThingToPrint in logArray[i]) {
-                                    myStringToPrint += ", " + myInterestingKeys[j].otherThingToPrint + ": " + logArray[i][myInterestingKeys[j].otherThingToPrint];
-                                }
-                            }
-                            console.log(myStringToPrint);
-                        }
-                    }
-                } else {
-                    // Here we handle Interesting Keys for which the key can have any value.
-                    myInterestingKeys[j].count += 1;
-                    // Always try to print the time, and print it first.
-                    if ("time" in logArray[i]) {
-                        myStringToPrint = logArray[i].time;
-                    }
-                    if (myInterestingKeys[j].hasKey in logArray[i]) {
-                        myStringToPrint += ", " + myInterestingKeys[j].hasKey + ": ";
-                        // Handle logArray payloads that are still objects (errors).
-                        if (typeof(logArray[i][myInterestingKeys[j].hasKey]) == "object") {
-                            myStringToPrint += JSON.stringify(logArray[i][myInterestingKeys[j].hasKey]);
-                        } else {
-                            myStringToPrint += logArray[i][myInterestingKeys[j].hasKey];
-                        }                            
-                        if ("otherThingToPrint" in myInterestingKeys[j]) {
-                            if (myInterestingKeys[j].otherThingToPrint in logArray[i]) {
-                                myStringToPrint += ", " + myInterestingKeys[j].otherThingToPrint + ": " + logArray[i][myInterestingKeys[j].otherThingToPrint];
-                            }
-                        }
-                    console.log(myStringToPrint);
-                    }
-                }
-            }
+    let myLogTypes = [];
+    for (let i = 0; i < logArray.length; i += 1) {
+        //console.log("current log entry is: ", JSON.stringify(logArray[i]));
+        let currentLogKeys = JSON.stringify(Object.keys(logArray[i]).sort());
+        //console.log("current log sorted keys are: ", currentLogKeys);
+        if (myLogTypes.indexOf(currentLogKeys) > -1) {
+            // currentLog is already mapped
+        } else {
+            // add the currentLog to the set
+            myLogTypes.push(currentLogKeys);
         }
     }
-    // Print final statistics
-    console.log("Input filename: ", process.argv[2]);
-    console.log("Interesting key counts:");
-    for (let j = 0; j < myInterestingKeys.length; j += 1) {
-        console.log(JSON.stringify(myInterestingKeys[j]));
+    console.log("My Log Types are:");
+    for (let j = 0; j < myLogTypes.length; j += 1) {
+        console.log(myLogTypes[j]);
     }
     console.log("Total number of log records processed was ", logArray.length);
     //console.log(logArray);
