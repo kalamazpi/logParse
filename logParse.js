0dfaf7ba1dd187c173bfa31efd9f34530450c128
Brent Wilson <kalamazpi@gmail.com>
2018-01-19 19:53:28 -0600

    Improved parsing algorithm.

--- logParse.js
+++ logParse.js
@@ -62,6 +62,8 @@ rl.on("line", function(line) {
 
 // On file close, we process the array and perform the work of the program.
 rl.on("close", function() {
+    let outArray = []; // output logArray matching criteria
+    /*
     let myLogTypes = [];
     for (let i = 0; i < logArray.length; i += 1) {
         //console.log("current log entry is: ", JSON.stringify(logArray[i]));
@@ -74,10 +76,52 @@ rl.on("close", function() {
             myLogTypes.push(currentLogKeys);
         }
     }
-    console.log("My Log Types are:");
-    for (let j = 0; j < myLogTypes.length; j += 1) {
-        console.log(myLogTypes[j]);
+    */
+    // Build an array of logs that match the required filter criteria.
+    // TODO: Update to handle nested objects in values.
+    let logFilter = [
+        //{"msg":"development env"},
+        //{"msg":"commit SET_AC_PRESENT"},
+        //{"farRed":300}
+        //{"payload":{"farRed":300,"blue":300,"red":300,"lime":300,"white":300,"pcbTemp":278,"temp1":228,"temp2":231,"bus60Voltage":57414,"bus60Current":670,"bus5PICVoltage":4981,"bus5BBVoltage":4959,"bus5SystemVoltage":4891,"bus12Voltage":12052,"fanSpeed":0,"fan1Tach":474,"fan2Tach":488,"overTempCutback":0,"runtimeSinceReset":102,"buttonStatus":0,"faultCode":20,"lastResetCode":64,"rtcTime":133402068,"status":4}}
+        {"msg":"commit SET_AC_PRESENT","payload":0}
+    ]
+    let count = 0;
+    for (let i = 0; i < logArray.length; i += 1) {
+        // if the current value of logArray[i] passes any filter criteria in logFilter,
+        // push it to outArray[].  Note that if a logFilter entry contains more than one
+        // key/value pair, all key/value pairs must match in order for log to pass.
+        for (let j = 0; j < logFilter.length; j += 1) {
+            let passed_filter = true;
+            for (let key in logFilter[j]) {
+                // if logArray entry doesn't have the property or if it has the property but
+                // the value doesn't match, the filter test will fail.
+                if (!logArray[i].hasOwnProperty(key) || (logArray[i].hasOwnProperty(key) && logArray[i][key] != logFilter[j][key])) {
+                    passed_filter = false;
+                }
+            }
+            if (passed_filter == true) {
+                outArray.push(logArray[i]);
+                count++;
+                break; // don't record this log more than once
+            }
+        }
     }
+    /*
+    outArray.forEach((logEntry) => {
+        console.log(JSON.stringify(logEntry));
+    });
+    */
+    let timeArray = [];
+    let firstTime_ms = Date.parse(outArray[0].time);
+    for (let logEntry in outArray) {
+        let time_ms = Date.parse(outArray[logEntry].time);
+        //console.log(JSON.stringify(outArray[logEntry]));
+        let delta_ms = time_ms - firstTime_ms;
+        timeArray.push(delta_ms);
+        console.log(outArray[logEntry].time + ", " + delta_ms / 1000);
+    }
+    console.log("Found " + count + " occurrences of log");
     console.log("Total number of log records processed was ", logArray.length);
     //console.log(logArray);
     rl.close();
